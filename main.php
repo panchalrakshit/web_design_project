a493bb2c5c9f13f722f1db177fabbe7e7ce10a56
mohit169 <mohitbhatt369@gmail.com>
2023-04-16 19:16:36 +0530

    menu added
    menu added, it will be same for every restaurant

--- main.php
+++ main.php
@@ -102,7 +102,7 @@ $result = $conn->query($sql);
     while($row=mysqli_fetch_assoc($result)){
     ?>
     <div class="listitems1">
-        <a href="menu.php?restaurant=<?php echo urlencode($row['restaurant_name']); ?>">
+        <a href="leelapalace.html">
             <div class="item1">
                 <div class="product"align="center">
                     <img width="150px" height="150px" src="<?php echo $row['r_image']; ?>" alt="<?php echo $row['restaurant_name']; ?>">
