0020101c508f7e2429fc91e42ca29ecba8f6141a
panchalrakshit <rakshitpanchal7890@gmail.com>
2023-04-20 13:47:58 +0530

    rr
    rr

--- main.php
+++ main.php
@@ -20,7 +20,7 @@ $result = $conn->query($sql);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylesheet.css">
-    <title>Dashboard | TASTE ON WAY</title>
+    <title>TASTE ON WAY | ORDER ASAP</title>
 </head>
 <body class="body">
     <div class="navbar">
